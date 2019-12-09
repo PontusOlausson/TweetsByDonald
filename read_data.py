08c7de213d55988eedcb1cb51c99205a67ba237c
christofferlinne <christoffer.linne@hotmail.com>
2019-12-09 17:24:29 +0100

    tjabba test

--- read_data.py
+++ read_data.py
@@ -5,7 +5,13 @@ class DataReader:
     def __init__(self, training_file):
 
         if training_file:
+            training_set = self.read_and_process_data(training_file)
 
+    def read_data(self, training_file):
+        pass
+
+    def process_data(self):
+        pass
 
 
 
